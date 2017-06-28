99cf8eaf8d610530693a4d62e465aa9827f9e1e9
lihe <lihe188@gmail.com>
2017-06-28 21:50:46 +0800

    http

--- src/main/webapp/src/app/app.component.ts
+++ src/main/webapp/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http }       from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import '../assets/css/styles.css';
 
@@ -7,4 +9,13 @@ import '../assets/css/styles.css';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+  constructor(private http:Http)  {}
+  ngOnInit(): void {
+    this.http.get('/hello')
+        .toPromise()
+        .then(response => console.log("res="+response));
+
+  }
+
+}
