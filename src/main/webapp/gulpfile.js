2592fbce1c59423d9f8ebe856661b5481e10903b
lihe <lihe@gmail.com>
2017-06-29 19:59:09 +0800

    采用gulp构建前端文件，管理webpack

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -1,23 +1,67 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 var clean = require('gulp-clean');
+var gutil = require("gulp-util");
+var _ = require('lodash');
+var webpack = require('webpack');
+var webpackConfig = require('./config/webpack.prod.js');
 
-gulp.task('clean-dist', function () {
+//清空打包目录
+gulp.task('clean:dist', function () {
     return gulp.src('dist/*.*', {read: false})
         .pipe(clean());
 });
 
-gulp.task('watch', function () {    // 这里的watch，是自定义的，写成live或者别的也行
+//监控前端文件修改，运行webpack打包
+gulp.task('build:watch',['webpack:build-dev'], function () {    // 这里的watch，是自定义的，写成live或者别的也行
     livereload.listen();
     // app/**/*.*的意思是 app文件夹下的 任何文件夹 的 任何文件
     gulp.watch('src/**/*.*', function (file) {
-        gulp.start('clean-dist');
-        setTimeout(function () {
+        gulp.start('webpack:build-dev', function () {
+            // 在webpack:build-dev运行完成后执行下面的代码，前提条件是在定义webpack:build-dev这个的时候，接收了一个毁掉函数function(callback)
             livereload.reload();
-        }, 3000);
+        });
     });
 });
 
+//webpack打包，在打包之前先清空打包目录
+gulp.task("webpack:build-dev", ['clean:dist'], function(callback) {
+    // run webpack
+    webpack(webpackConfig).run(function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack:build-dev", err);
+        gutil.log("[webpack:build-dev]", stats.toString({
+            colors: true
+        }));
+        callback();
+    });
+});
+
+// gulp.task("build-dev", ["webpack:build-dev"], function() {
+//     gulp.watch(["src/**/*"], ["webpack:build-dev"]);
+// });
+
 gulp.task('default', function() {
-    gulp.start('watch');
+    gulp.start('build:watch');
+});
+
+
+
+
+//
+gulp.task('webpack', ['clean:dist'], function(callback) {
+    webpack(webpackConfig, function(err, stats) {
+        console.log("err="+err+",stats="+stats);
+        callback();
+        console.log("webpack succeed");
+    });
+});
+
+// 监听模式
+gulp.task('watch:webpack', ['clean:dist'], function() {
+    var watchConfig = _.merge(webpackConfig, { watch: true });
+    webpack(watchConfig).watch(200, function(err, stats) {
+        console.log("err="+err+",stats="+stats);
+        console.log("webpack succeed");
+
+    });
 });
